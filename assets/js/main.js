66b3b3ed6551b477f841aadca0b2fe25106c1a19
glbdmn <gabrieledamiani@hotmail.it>
2023-04-20 17:23:27 +0200

    fine eserc con bonus 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,24 @@ createApp({
         creaElemento(){
 
             if( this.input != ''){
-                this.todos.push(this.input)
-            }
+                const input = {
+                    text: this.input,
+                    done: false
+                }
+                this.todos.push( input );
+                this.input = ''
+            }    
+      
         },
         cancellazione(i){
             this.todos.splice( i, 1 )
+        },
+        inVerti(i){
+           if(this.todos[i].done == false) {
+            this.todos[i].done = true
+           } else if (this.todos[i].done == true) {
+             this.todos[i].done = false
+           }
         }
             
            
@@ -36,3 +49,6 @@ createApp({
     
     }
 }).mount('#app')
+
+
+
